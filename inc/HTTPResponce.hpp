fb11dad92818ace5907d733e477867a334d340d7
onixyara <onixyarka@gmail.com>
2023-10-13 15:34:56 +0300

    HTTPResponce added

---
+++ inc/HTTPResponce.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <sstream>
+
+class HTTPResponse
+{
+public:
+    int status;
+    std::string statusText;
+    std::map<std::string, std::string> headers;
+    std::string body;
+
+    std::string toString() const {
+        std::ostringstream response;
+        response << "HTTP/1.1 " << status << " " << statusText << "\r\n";
+        for (const auto& pair : headers) {
+            response << pair.first << ": " << pair.second << "\r\n";
+        }
+        response << "\r\n";
+        response << body;
+        return response.str();
+    }
+};
